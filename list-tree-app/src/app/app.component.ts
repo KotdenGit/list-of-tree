96785998801c5314c8c9c2d0621f580232269754
Yury V <kotden09@gmail.com>
2025-06-20 13:21:17 +0300

    feat: update TreeView component to handle treeData input and adjust app component structure

--- list-tree-app/src/app/app.component.ts
+++ list-tree-app/src/app/app.component.ts
@@ -4,20 +4,10 @@ import { TreeViewComponent } from './tree-view/tree-view.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TreeViewComponent],
+  imports: [TreeViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'list-tree-app';
-  treeData = {
-    name: 'Root',
-    children: [
-      { name: 'Child 1' },
-      {
-        name: 'Child 2',
-        children: [{ name: 'Grandchild 1' }, { name: 'Grandchild 2' }],
-      },
-    ],
-  };
 }
--- list-tree-app/src/app/tree-view/tree-view.component.ts
+++ list-tree-app/src/app/tree-view/tree-view.component.ts
@@ -1,10 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tree-view',
   templateUrl: './tree-view.component.html',
   styleUrl: './tree-view.component.scss',
+  imports: [CommonModule],
 })
 export class TreeViewComponent {
-  @Input() treeData: any;
+  @Input() treeData: any = {
+    name: 'Root',
+    children: [
+      { name: 'Child 1' },
+      {
+        name: 'Child 2',
+        children: [{ name: 'Grandchild 1' }, { name: 'Grandchild 2' }],
+      },
+    ],
+  };
 }
