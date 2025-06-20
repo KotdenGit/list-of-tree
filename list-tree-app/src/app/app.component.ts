82691d302f3b0aef0cd9c589322416988b28ace2
Yury V <kotden09@gmail.com>
2025-06-20 12:56:30 +0300

    feat: implement TreeView component and integrate with app component

--- list-tree-app/src/app/app.component.ts
+++ list-tree-app/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TreeViewComponent } from './tree-view/tree-view.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TreeViewComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'list-tree-app';
+  treeData = {
+    name: 'Root',
+    children: [
+      { name: 'Child 1' },
+      {
+        name: 'Child 2',
+        children: [{ name: 'Grandchild 1' }, { name: 'Grandchild 2' }],
+      },
+    ],
+  };
 }
